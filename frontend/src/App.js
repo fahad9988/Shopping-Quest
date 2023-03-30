194673f935f473b4c13be63f5d8cf686506156f9
Kirtiraj Ingale <kirtiraingale46@gmail.com>
2023-03-30 08:02:40 +0530

    Admin panel structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AllRoutes from './AllRoutes/AllRoutes';
+//import AdminPage from "./Pages/AdminPage/Dashboard"
 
 function App() {
   return (
     <div className="App">
       <AllRoutes />
+      {/* <AdminPage/> */}
     </div>
   );
 }
---
+++ frontend/src/Pages/AdminPage/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <>
+      Admin DashBoard
+    </>
+  )
+}
+
+export default Dashboard
